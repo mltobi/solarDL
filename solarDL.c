c0f84415741aec57fc2f9a3b64c5b4bb79339e2a
mltobi <65978622+mltobi@users.noreply.github.com>
2022-03-12 20:58:03 +0100

    Update solarDL.c

--- solarDL.c
+++ solarDL.c
@@ -337,7 +337,7 @@ int main(int argc, char *argv[])
       strcat(strpre, strpost);
 
       file = fopen( pathHtmlUpdate, "w" );
-      fprintf(file, "%s\n", str);
+      fprintf(file, "%s\n", strpre);
       fclose(file);
 
       printf("%04d%02d%02d %02d:%02d:%02d - ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
